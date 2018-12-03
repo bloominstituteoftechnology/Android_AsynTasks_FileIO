986b2ab5d24dc484311efc8858133b352d01611b
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-03 14:46:56 -0800

    Implemented reading and displaying files. Implemented saving files and adding them to the dropdown.

--- app/src/main/java/com/joshuahalvorson/android_asynctasks_fileio/MainActivity.java
+++ app/src/main/java/com/joshuahalvorson/android_asynctasks_fileio/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +15,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private ProgressBar progressBar;
     private EditText shiftTextValue;
     private AsyncTask cipherTextTask;
+    private Spinner spinner;
     Context context;
     String[] items;
 
@@ -41,11 +47,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         cipherTextView = findViewById(R.id.cipher_text);
         shiftTextValue = findViewById(R.id.shift_text_value);
         context = this;
-    }
+        spinner = findViewById(R.id.file_select);
+        createAdapter();
 
-    @Override
-    protected void onResume() {
-        super.onResume();
         findViewById(R.id.decode_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,12 +58,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 cipherTextTask = (new CipherTextTask()).execute(cipherText, shiftValue);
             }
         });
-        Spinner spinner = findViewById(R.id.file_select);
-        List<String> spinnerArray =  new ArrayList<String>();
+
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fileName = shiftTextValue.getText().toString() + spinner.getSelectedItem().toString();
+                FileWriter fileWriter =  null;
+                try {
+                    File file = File.createTempFile(fileName, null, context.getCacheDir());
+                    fileWriter = new FileWriter(file);
+                    fileWriter.write(cipherTextView.getText().toString());
+                    createAdapter();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(fileWriter != null){
+                        try {
+                            fileWriter.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void createAdapter(){
+        List<String> spinnerArray =  new ArrayList<>();
         try {
             String[] files = getAssets().list("");
             for(String s : files){
-                if(s.substring(s.length() - 3).equals("txt")){
+                if(s.substring(s.length() - 4).equals(".txt")){
+                    spinnerArray.add(s);
+                }
+            }
+            String[] cacheFiles = context.getCacheDir().list();
+            for(String s : cacheFiles){
+                if(s.contains(".txt")){
                     spinnerArray.add(s);
                 }
             }
@@ -67,12 +103,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             e.printStackTrace();
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
 
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerArray);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                BufferedReader bufferedReader;
+                StringBuilder sb = new StringBuilder();
+                try{
+                    String file = adapter.getItem(position);
+                    if(file.contains(".tmp")){
+                        File newFile = new File(context.getCacheDir(), file);
+                        FileReader fileReader = new FileReader(newFile);
+                        bufferedReader = new BufferedReader(fileReader);
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            sb.append(line);
+                            sb.append("\n");
+                        }
+                        cipherTextView.setText(sb.toString());
+                    }else{
+                        InputStream inputStream = context.getAssets().open(file);
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        bufferedReader = new BufferedReader(inputStreamReader);
+                        while(bufferedReader.readLine() != null){
+                            sb.append(bufferedReader.readLine());
+                        }
+                        cipherTextView.setText(sb.toString());
+                    }
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -127,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                             newString.append(strings[0].charAt(i));
                         }
                     }
-                    if(lettersShifted % 40 == 0){
+                    if(lettersShifted % 30 == 0){
                         publishProgress(lettersShifted);
                         if(isCancelled()){
                             progressBar.setVisibility(View.GONE);
