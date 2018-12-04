048f88a5e4b574e5bbe46346f2de9ed0c517c63b
Jacob Seppanen <jbseppanen@gmail.com>
2018-12-03 18:47:27 -0800

    Code cleanup.

--- app/src/main/java/com/example/jacob/android_threads/MainActivity.java
+++ app/src/main/java/com/example/jacob/android_threads/MainActivity.java
@@ -20,7 +20,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -282,29 +281,5 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-    }
-
-
-    public static String pi_digits(int digits) {
-        int SCALE = 10000;
-        int ARRINIT = 2000;
-        StringBuffer pi = new StringBuffer();
-        int[] arr = new int[digits + 1];
-        int carry = 0;
-
-        for (int i = 0; i <= digits; ++i)
-            arr[i] = ARRINIT;
-
-        for (int i = digits; i > 0; i -= 14) {
-            int sum = 0;
-            for (int j = i; j > 0; --j) {
-                sum = sum * j + SCALE * arr[j];
-                arr[j] = sum % (j * 2 - 1);
-                sum /= j * 2 - 1;
-            }
-            pi.append(String.format("%04d", carry + sum / SCALE));
-            carry = sum % SCALE;
-        }
-        return pi.toString();
     }
 }
